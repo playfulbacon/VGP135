ba85191e514018726cc69029caaaa204e73d1358
AndyBacon <bacon.andrew@gmail.com>
2019-10-25 09:27:26 -0700

    Init project.

---
+++ VGP135_A1_Golf/Assets/Ball.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    Rigidbody rb;
+    bool isPressed = false;
+    bool isDragging = false;
+    public Transform aimPrefab;
+    Vector3 hitDirection;
+    float hitForce = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        aimPrefab = Instantiate(aimPrefab);
+        aimPrefab.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 groundHit = hit.point;
+                groundHit.y = transform.position.y;
+
+                if (Vector3.Distance(transform.position, groundHit) > 0.5f)
+                {
+                    isDragging = true;
+                    aimPrefab.gameObject.SetActive(true);
+
+                    hitDirection = -(groundHit - transform.position).normalized;
+                    aimPrefab.transform.forward = hitDirection;
+                    aimPrefab.position = transform.position + hitDirection * 1.25f;
+                }
+            }
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        rb.isKinematic = true;
+        isPressed = true;
+    }
+    public void OnMouseUp()
+    {
+        rb.isKinematic = false;
+        isPressed = false;
+        aimPrefab.gameObject.SetActive(false);
+
+        if (isDragging)
+            rb.AddForce(hitDirection * hitForce);
+
+        isDragging = false;
+
+    }
+}
