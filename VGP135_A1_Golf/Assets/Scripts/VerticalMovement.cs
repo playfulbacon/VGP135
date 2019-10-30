50d7543fbe70baa84db163f5039d48a0e3879176
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-10-29 23:50:04 -0700

    [Flavio] -Adding Special Level 1 and 2

---
+++ VGP135_A1_Golf/Assets/Scripts/VerticalMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMovement : MonoBehaviour
+{
+    public float startDelay;
+    public float timer;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = startDelay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= (Time.deltaTime * 1);
+
+        if( timer < 0.0f)
+        {
+            Vector3 yMove = new Vector3(0, speed * Time.deltaTime, 0);
+            gameObject.transform.position -= yMove;
+
+        }
+    }
+}
