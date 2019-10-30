5533b70a87eed12406a70aa7b5e2c817b01b047e
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-10-30 13:37:04 -0700

    [Flavio] - Adding spring feature

---
+++ VGP135_A1_Golf/Assets/Scripts/SpringMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringMovement : MonoBehaviour
+{
+    SpringJoint springJoint;
+    Rigidbody rigidbody;
+    bool startTimer;
+    [Tooltip("How long does it need to wait for applying velocity")]
+    public float delayTime;
+    public float forceToApply;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        springJoint = GetComponent<SpringJoint>();
+        rigidbody = GetComponent<Rigidbody>();
+        timer = delayTime;
+        startTimer = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startTimer)
+            timer -= (Time.deltaTime * 1.0f);
+        if (rigidbody)
+        {
+            if (rigidbody.velocity.x < 0.2f && rigidbody.velocity.x > -0.2f && !startTimer)
+                startTimer = true;
+
+            if(timer < 0.0f)
+            {
+                Debug.Log("Apply foce");
+                rigidbody.velocity -= new Vector3(forceToApply, 0.0f);
+                startTimer = false;
+                timer = delayTime;
+            }
+        }
+
+        //Debug.Log("Spring Current force " + springJoint.currentForce);
+        Debug.Log("Obj velocity =  " + rigidbody.velocity);
+    }
+}
