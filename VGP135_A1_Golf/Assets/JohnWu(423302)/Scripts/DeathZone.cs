3190054ca8aecec48e658ee23137852e8691b558
WuSiyuan <423302@AIV.EDMC.STU>
2019-11-01 10:22:47 -0700

    Add my own folder

---
+++ VGP135_A1_Golf/Assets/JohnWu(423302)/Scripts/DeathZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DeathZone : MonoBehaviour
+{
+    public string startScene;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        FindObjectOfType<GoalMenu>().SetGoalMenu(true);
+    }
+}
+
