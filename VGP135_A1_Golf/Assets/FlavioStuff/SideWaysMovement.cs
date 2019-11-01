bfa4f78f009ba300a5160bec5c90f11a189d59e0
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-11-01 11:33:28 -0700

    FlavioStuff

---
+++ VGP135_A1_Golf/Assets/FlavioStuff/SideWaysMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWaysMovement : MonoBehaviour
+{
+    public Transform startPoint;
+    public Transform endPoint;
+    public float speed;
+    public Transform currentFolloPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(endPoint)
+            currentFolloPoint = endPoint;
+
+        if (startPoint)
+            currentFolloPoint = startPoint;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime; // calculate distance to move
+
+
+        Debug.Log("Move towards start point");
+        transform.position = Vector3.MoveTowards(transform.position, currentFolloPoint.position, step);
+
+        //Creating range when objects is close to end point
+        if (transform.position.x < endPoint.position.x + 0.8f && 
+            transform.position.x > endPoint.position.x - 0.8f)
+        {
+            Debug.Log("Move towards start point");
+            currentFolloPoint = startPoint;
+        }
+
+        //Creating range when objects is close to end point
+        if (transform.position.x < startPoint.position.x + 0.8f &&
+            transform.position.x > startPoint.position.x - 0.8f)
+        {
+            Debug.Log("Move towards end point");
+            currentFolloPoint = endPoint;
+        }
+    }
+}
