8a09c06f8931783409305dac444885dfff1d7768
YuYang <y3y10309@gmail.com>
2019-11-01 12:25:13 -0700

    -Add my folder to Students folder

---
+++ VGP135_A1_Golf/Assets/Students/YuYang_Tim/My_Scripts/BoardTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Board;
+    
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        Board.transform.Rotate(-20.0f, 0.0f, 0.0f);
+    }
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Board.transform.Rotate(45.0f, 0.0f, 0.0f);
+    //}
+}
---
+++ VGP135_A1_Golf/Assets/Students/YuYang_Tim/My_Scripts/MoveGoal.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGoal : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int awarenessRadius = 5;
+    public bool isAwared = false;
+    public float speed = -1.0f;
+    public GameObject player;
+    bool isActive = true;
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    public virtual void OnHit()
+    {
+        isActive = false;
+        Debug.Log("1");
+        FindObjectOfType<GoalMenu>().SetGoalMenu(true);
+    }
+
+    void Update()
+    {
+        if (player != null && (isActive))
+        {
+            // Awareness
+            isAwared = Vector3.Magnitude(transform.position - player.transform.position) < awarenessRadius;
+            if (isAwared)
+            {
+                Vector3 lookAt = (player.transform.position - transform.position);
+                lookAt.y = 0.0f;
+
+                // If player is in range will move towards them
+
+                    // Apply force
+                   
+                    transform.position = (transform.position + lookAt * speed);
+                if (speed < 0)
+                {
+                    speed += 0.005f;
+                }
+                if(speed>0)
+                {
+                    speed = 0;
+                }
+               
+                // If in range will attack
+
+                lookAt = transform.forward + lookAt * 0.1f;
+                transform.forward = Vector3.Normalize(lookAt);
+            }
+        }
+    }
+
+
+}
