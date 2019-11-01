f9c79ee8844dac3c524d678ce014aca529559c1a
YangYu <1820201@AIV.EDMC.STU>
2019-11-01 11:13:45 -0700

    Add my folder

---
+++ VGP135_A1_Golf/Assets/YuYang_Tim/My_Scripts/GoalMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalMenu : MonoBehaviour
+{
+    public string levelSelectSceneName;
+    public GameObject goalMenuHolder;
+
+    void Start()
+    {
+        SetGoalMenu(false);
+    }
+
+    public void SetGoalMenu(bool value)
+    {
+        goalMenuHolder.SetActive(value);
+    }
+
+    public void PlayAgainButtonDown()
+    {
+        Debug.Log("3");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void LevelSelectButtonDown()
+    {
+        SceneManager.LoadScene(levelSelectSceneName);
+    }
+}
---
+++ VGP135_A1_Golf/Assets/YuYang_Tim/My_Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string startScene;
+
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+}
