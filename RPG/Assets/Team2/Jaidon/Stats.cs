7a4a2a8266fb4f173617612f17d46a9e2edf622d
Jaidon van Herwaarden <jaidon.vanherwaarden@gmail.com>
2019-11-15 11:48:32 -0800

    Started the Stats script for keeping, fetching, and modifying stats for a character

---
+++ RPG/Assets/Team2/Jaidon/Stats.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    // Basic Stats
+    int baseStrength;
+    int baseIntelligence;
+    int baseConstitution;
+    int baseWisdom;
+    int baseDexterity;
+
+    // Public attack function (given an enemy)
+    // Public XP gain function
+
+    void Start()
+    {
+        // Based on class, modify stats
+    }
+
+    // Returns the base Strength score before modifiers
+    public int GetBaseStrength()
+    {
+        return baseStrength;
+    }
+    // Returns the base Intelligence score before modifiers
+    public int GetBaseIntelligence()
+    {
+        return baseIntelligence;
+    }
+    // Returns the base Constitution score before modifiers
+    public int GetBaseConstitution()
+    {
+        return baseConstitution;
+    }
+    // Returns the base Wisdom before score modifiers
+    public int GetBaseWisdom()
+    {
+        return baseWisdom;
+    }
+    // Returns the base stat before score modifiers
+    public int GetBaseDexterity()
+    {
+        return baseDexterity;
+    }
+    public float GetAttackSpeed()
+    {
+        // Returns the time it waits before making another attack
+        return (float)(1 / baseDexterity);
+    }
+}
