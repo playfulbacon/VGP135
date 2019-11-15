4365ba20f723b53253d3354bb9e10502d32b086c
Peter Mingzhuo Zhang <zhangpeter96@hotmail.com>
2019-11-15 10:59:14 -0800

    [Team1]--- Add PlayerMovement.cs Add CameraController.cs Set up a test scene

---
+++ RPG/Assets/Students/Team1/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+// Required Function from other team
+// [Character Status] GetMovementSpeed()
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 5.0f;
+    private NavMeshAgent agent;
+
+    void Awake()
+    {
+        // speed = GetComponent<CharacterStatus>().GetMovementSpeed();
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 groundHit = hit.point;
+                agent.destination = groundHit;
+            }
+        }
+
+    }
+}
