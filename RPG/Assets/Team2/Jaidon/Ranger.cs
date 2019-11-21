ca986797f7d75a447f7f6884035ba4f894c04683
Jaidon <jaidon.vanherwaarden@gmail.com>
2019-11-20 17:52:43 -0800

    Added extra to Stats script

--- RPG/Assets/Team2/Jaidon/Ranger.cs
+++ RPG/Assets/Team2/Jaidon/Ranger.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Ranger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int currentExperience = 0;
+    int nextLevelExperienceRequired = 10;
+
+    public int dexterityOnLevelUp = 2;
+    public int strengthOnLevelUp = 1;
+
+
+    // Public attack function (given an enemy)
+    // Public XP gain function
+
+    public void Start()
     {
-        
+        stats = this.GetComponent<Stats>;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LevelUpStats()
     {
-        
+        ModifyBaseStrength(strengthOnLevelUp);
+        ModifyBaseDexterity(dexterityOnLevelUp);
+
+        // Allow user to level up as well
+
+    }
+    public void GiveExperience(int xpAmount)
+    {
+        currentExperience += xpAmount;
+        if (currentExperience >= nextLevelExperienceRequired)
+        {
+            LevelUpStats();
+        }
+    }
+    public void AutoAttack(GameObject enemyTarget)
+    {
+
     }
 }
--- RPG/Assets/Team2/Jaidon/Stats.cs
+++ RPG/Assets/Team2/Jaidon/Stats.cs
@@ -1,52 +1,138 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
     // Basic Stats
-    int baseStrength;
-    int baseIntelligence;
-    int baseConstitution;
-    int baseWisdom;
-    int baseDexterity;
+    int baseStrength        = 10;
+    int currentStrength;
 
-    // Public attack function (given an enemy)
-    // Public XP gain function
+    int baseIntelligence    = 10;
+    int currentIntelligence;
 
-    void Start()
-    {
-        // Based on class, modify stats
-    }
+    int baseConstitution    = 10;
+    int currentConstition;
 
-    // Returns the base Strength score before modifiers
+    int baseWisdom          = 10;
+    int currentWisdom;
+
+    int baseDexterity       = 10;
+    int currentDexterity;
+
+
+    // Secondary Stats
+    float physicalDamage = 100;
+    float attackSpeed = 0.1f;
+
+    float spellDamage = 100;
+    int maxMagicPoints = 100;
+    int currentMagicPoints = 100;
+
+    float maxHealth = 100;
+    float currentHealth = 100;
+
+
+    /// Strength Functions ----------------------------------------------------------
     public int GetBaseStrength()
     {
         return baseStrength;
     }
-    // Returns the base Intelligence score before modifiers
+    public int GetCurrentStrength()
+    {
+        return currentStrength;
+    }
+    public void ModifyBaseStrength(int n)
+    {
+        baseStrength += n;
+        CalculatePhysicalDamage();
+    }
+    void CalculatePhysicalDamage()
+    {
+        physicalDamage = 100 + (currentStrength * 5);      // % based damage. Each point increases physical damage by 5%
+    }
+
+
+
+    /// Intelligence Functions ------------------------------------------------------
     public int GetBaseIntelligence()
     {
         return baseIntelligence;
     }
+    public int GetCurrentIntelligence()
+    {
+        return currentIntelligence;
+    }
+    public void ModifyBaseIntelligence(int n)
+    {
+        baseIntelligence += n;
+
+    }
+    void CalculateSpellDamage()
+    {
+        spellDamage = 100 + (currentIntelligence * 5);      // % based damage. Each point increases spell damage by 5%
+    }
+
+
+
+    /// Constitution Functions ------------------------------------------------------
     // Returns the base Constitution score before modifiers
     public int GetBaseConstitution()
     {
         return baseConstitution;
     }
+    public int GetCurrentConstitution()
+    {
+        return currentConstition;
+    }
+    public void ModifyBaseConstitution(int n)
+    {
+        baseConstitution += n;
+        CalculateMaxHealth();
+    }
+    void CalculateMaxHealth()
+    {
+        maxHealth = 100 + (baseConstitution * 10); // Increase by 10 health per constitution point
+    }
+
+
+
+    /// Wisdom Functions ------------------------------------------------------------
     // Returns the base Wisdom before score modifiers
     public int GetBaseWisdom()
     {
         return baseWisdom;
     }
-    // Returns the base stat before score modifiers
+    public int GetCurrentWisdom()
+    {
+        return currentWisdom;
+    }
+    public void ModifyBaseWisdom(int n)
+    {
+        baseWisdom += n;
+        CalculateMagicPoints();
+    }
+    void CalculateMagicPoints()
+    {
+        maxMagicPoints = 100 + (currentWisdom * 10);
+    }
+
+
+
+    /// Dexterity Functions ---------------------------------------------------------
+    // Returns the base Dexterity stat before score modifiers
     public int GetBaseDexterity()
     {
         return baseDexterity;
     }
-    public float GetAttackSpeed()
+    public void ModifyBaseDexterity(int n)
     {
-        // Returns the time it waits before making another attack
-        return (float)(1 / baseDexterity);
+        baseDexterity += n;
+    }
+    // Returns the attackspeed of the character based off of dexterity
+    void CalculateAttackSpeed()
+    {
+        attackSpeed = 10 / currentDexterity;
     }
 }
