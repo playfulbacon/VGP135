b302bb1088ec1eea4380178b4b04ae22e8ec56fd
JohnWu <wusiyuan54111@gmail.com>
2019-11-25 13:34:44 -0800

    [MingzhuoZhang]--- Add Models, Materials, Textures folders in to the team1 folder for character model. Add Animations folder for character animation

---
+++ RPG/Assets/Teams/Team1_CameraMovement/Scripts/PlayerAnimationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator mAnimator;
+
+    private void Awake()
+    {
+        mAnimator = GetComponentInChildren<Animator>();
+        Assert.IsNotNull(mAnimator, "[PlayerAnimationController]--- mAnimator is null");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //mAnimator.transform
+    }
+}
