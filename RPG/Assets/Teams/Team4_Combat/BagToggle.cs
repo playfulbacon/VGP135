784dc09fd15e72dd7b83151f5d560fb67f5ef7f0
William Chi <williambillchi@hotmail.com>
2019-11-29 07:18:12 -0800

    [William] added bag toggle

---
+++ RPG/Assets/Teams/Team4_Combat/BagToggle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagToggle : MonoBehaviour
+{
+    bool isBagOpen;
+    Vector3 toggleVec;
+    float toggleX;
+    void Start()
+    {
+        toggleVec = new Vector3();
+        isBagOpen = true;
+        toggleX = 1500;
+        ToggleBag();
+        transform.Translate(new Vector3(0f, -150f, 0f));
+    }
+
+    public void ToggleBag()
+    {
+        if (isBagOpen)
+        {
+            toggleVec.x = toggleX;
+            transform.Translate(toggleVec);
+            isBagOpen = false;
+        }
+        else
+        {
+            toggleVec.x = -toggleX;
+            transform.Translate(toggleVec);
+            isBagOpen = true;
+        }
+    }
+}
