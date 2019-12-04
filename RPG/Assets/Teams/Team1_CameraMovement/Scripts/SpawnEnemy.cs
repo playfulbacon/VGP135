c78141cb8ac35fa5a21a7c5814d4153de3d451db
Cyro Paulino da Costa Jr <cpcmetal@hotmail.com>
2019-12-03 18:58:30 -0800

    - Create SpawnEnemy script to generate random enemies position on map.

---
+++ RPG/Assets/Teams/Team1_CameraMovement/Scripts/SpawnEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public int TotalSpawn = 100;
+    public GameObject Enemy;
+    private List<GameObject> mListEnemies;
+    
+    void Start()
+    {
+        mListEnemies = new List<GameObject>(TotalSpawn);
+        for (int i = 0; i < TotalSpawn; ++i)
+        {
+            mListEnemies.Add(Enemy);
+        }
+        SpwanEnemies(TotalSpawn);
+    }
+
+    private void SpwanEnemies(int total)
+    {
+        Vector3 spawnPoints;
+        for (int i = 0; i < TotalSpawn; ++i)
+        {
+        }
+    }
+}
