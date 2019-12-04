83df1797d473da24625a2d3719ab0b156ed395bb
Cyro Paulino da Costa Jr <cpcmetal@hotmail.com>
2019-12-03 22:36:46 -0800

    Update script

--- RPG/Assets/Teams/Team1_CameraMovement/Scripts/SpawnEnemy.cs
+++ RPG/Assets/Teams/Team1_CameraMovement/Scripts/SpawnEnemy.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
     public int TotalSpawn = 100;
+    public GameObject terrain;
+    public Vector3 spawnRange;
     public GameObject Enemy;
     private List<GameObject> mListEnemies;
     
@@ -20,9 +22,15 @@ public class SpawnEnemy : MonoBehaviour
 
     private void SpwanEnemies(int total)
     {
-        Vector3 spawnPoints;
+        Vector3 spawnPosition;
         for (int i = 0; i < TotalSpawn; ++i)
         {
+            spawnPosition = new Vector3(Random.Range(-spawnRange.x / 2 + obstacleWidth, spawnRange.x / 2 - obstacleWidth),
+                                              terrain.transform.position.y + (obstacleHeight / 2),
+                                              Random.Range(-spawnRange.z / 2 + obstacleWidth, spawnRange.z / 2 - obstacleWidth));
+
+
+            Instantiate(Enemy, spawnPosition, Quaternion.identity);;
         }
     }
 }
