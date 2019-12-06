2b73f3b5f95199404a3e4931d620e7f7e6662919
Aisle Neral <aisle.neral@hotmail.com>
2019-12-06 02:29:19 -0800

    candycrush

---
+++ RPG/Assets/Scripts/DestroyParticle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyParticle : MonoBehaviour {
+
+    private float delayTimer = 1;
+
+	void Update () {
+        delayTimer -= Time.deltaTime;
+        if(delayTimer<= 0){
+            Destroy(this.gameObject);
+        }
+	}
+}
---
+++ RPG/Assets/Scripts/SwipeController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeController : MonoBehaviour {
+
+    public Vector2 firstPosition;
+    private Vector2 secondPosition;
+    public Vector2 moveDirection;
+    public bool isControlling = false;
+
+
+	void Update () {
+        if(Input.GetMouseButtonDown(0)){
+            isControlling = true;
+            firstPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if(Input.GetMouseButtonUp(0)){
+            isControlling = false;
+            secondPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moveDirection = new Vector2(secondPosition.x - firstPosition.x, secondPosition.y - firstPosition.y);
+        }
+	}
+}
