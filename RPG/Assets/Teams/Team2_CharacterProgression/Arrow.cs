f01c55b240bbb101d9caf56c3c2ec2bc90b9ada3
Amy <amy.rh.zhao@gmail.com>
2019-12-13 10:30:59 -0800

    - Added Raycast Check in AttackManager to avoid shooting obstacles - Fixed arrow shoot position - Added Enemy tag - Added auto destory after certain time on arrow

--- RPG/Assets/Teams/Team2_CharacterProgression/Arrow.cs
+++ RPG/Assets/Teams/Team2_CharacterProgression/Arrow.cs
@@ -8,21 +8,34 @@ public class Arrow : MonoBehaviour
     public GameObject Target { set { target = value; } }
     public float moveSpeed = 10.0f;
     public int damage;
+    public float destroyTimer = 3.0f;
+    private bool destroy = false;
 
     // Update is called once per frame
     void Update()
     {
+        if (destroy)
+        {
+            Destroy(gameObject);
+        }
+        destroyTimer -= Time.deltaTime;
+        if (destroyTimer <= 0.0f)
+        {
+            destroy = true;
+        }
         transform.position += transform.forward * moveSpeed * Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-
-        Enemy enemy = other.GetComponentInParent<Enemy>();
-        if (enemy)
+        if (!other.gameObject.CompareTag("Player"))
         {
-            enemy.TakeDamage(damage);
+            Enemy enemy = other.GetComponentInParent<Enemy>();
+            if (enemy)
+            {
+                enemy.TakeDamage(damage);
+            }
+            destroy = true;
         }
     }
 }
