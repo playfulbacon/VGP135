b1df73e0de3e2a9af6b8558a9b12a12d5ed61b9b
Flavio_L <floaiza40@gmail.com>
2019-12-20 00:56:43 -0800

    -Adding Winning audio clips -Adding Wining particle system

--- VGP135_A1_Golf/Assets/Scripts/Goal.cs
+++ VGP135_A1_Golf/Assets/Scripts/Goal.cs
@@ -5,13 +5,28 @@ using UnityEngine.UI;
 
 public class Goal : MonoBehaviour
 {
+    public ParticleSystem winningEffect;
+    public AudioClip winningClip;
+    public AudioSource audioSource;
     void Start()
     {
- 
+        if(audioSource && winningClip)
+            audioSource.clip = winningClip;
     }
 
     public virtual void OnHit()
     {
         FindObjectOfType<GoalMenu>().SetGoalMenu(true);
+        if (winningEffect)
+        {
+            //Playing particles
+            var  particles = Instantiate<ParticleSystem>(winningEffect, gameObject.transform);
+            particles.Play();
+            //Playing music
+            if (audioSource.clip)
+            {
+                audioSource.Play();
+            }
+        }
     }
 }
